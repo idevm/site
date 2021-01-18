dc3a819d77fe23cade8644cff5da9cf3b49840bf
idevm <77122241+idevm@users.noreply.github.com>
2021-01-18 23:38:02 +0300

    реализация контроллера

--- battleships/battleships.js
+++ battleships/battleships.js
@@ -29,16 +29,16 @@ var model = {
 			if (index >= 0){
 				ship.hits[index] = 'hit';
 				view.displayHit(guess);
-				view.displayMessage('HIT!');
+				view.displayMessage('РАНИЛ!');
 				if (this.isSunk(ship)){
-					view.displayMessage('You sank my battleship!');
+					view.displayMessage('УБИЛ!');
 					this.shipsSunk++;
 				}
 				return true;
 			}
 		}
 		view.displayMiss(guess);
-		view.displayMessage('You missed');
+		view.displayMessage('МИМО!');
 		return false;
 	},
 	isSunk: function (ship){
@@ -50,3 +50,57 @@ var model = {
 		return true;
 	}
 };
+var controller = {
+	guesses: 0,
+	processGuess: function (guess){
+		var location = parseGuess(guess);
+		if (location){
+			this.guesses++;
+			var hit = model.fire(location);
+			if (hit && model.shipsSunk === model.numShips){
+				view.displayMessage('Игра окончена. Вы потопили все корабли за ' 
+					+ this.guesses + ' выстрелов');
+				var end = document.getElementById('guessInput');
+				end.removeAttribute('id');
+			}
+		}
+	}
+};
+function parseGuess(guess){
+	var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+	if (guess === null || guess.length !== 2){
+		alert ('Нужно ввести букву и цифру');
+	} else {
+		firstChar = guess.charAt(0);
+		var row = alphabet.indexOf(firstChar.toUpperCase());
+		var column = guess.charAt(1);
+		if (isNaN(row) || isNaN(column)){
+			alert ('Недопустимое значение ввода');
+		} else if (row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize){
+			alert ('Не правильно указаны координаты!');
+		} else {
+			return row + column;
+		}
+	}
+	return null;
+};
+function init (){
+	var fireButton = document.getElementById('fireButton');
+	fireButton.onclick = handleFireButton;
+	var guessInput = document.getElementById('guessInput');
+	guessInput.onkeypress = handleKeyPress;
+};
+function handleFireButton (){
+	var guessInput = document.getElementById('guessInput');
+	var guess = guessInput.value;
+	controller.processGuess(guess);
+	guessInput.value = '';
+};
+function handleKeyPress (e){
+	var fireButton = document.getElementById('fireButton');
+	if (e.keyCode === 13){
+		fireButton.click();
+		return false;
+	}
+};
+window.onload = init;
